0c2658f72e8ad511ea45b9547a39b01b269abfc0
sueyan9 <409133742@qq.com>
2025-06-09 22:41:47 +1200

    css adjust

--- src/components/ProjectCarousel.jsx
+++ src/components/ProjectCarousel.jsx
@@ -1,3 +1,4 @@
+// ProjectCarousel.jsx
 import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -5,9 +6,18 @@ import "slick-carousel/slick/slick-theme.css";
 import "./ProjectCarousel.css";
 
 const projects = [
-    { title: "Project One", desc: "A cool project about ...", image: "/img1.png", link: "#" },
-    { title: "Project Two", desc: "Another project ...", image: "/img2.png", link: "#" },
-    { title: "Project Three", desc: "Yet another ...", image: "/img3.png", link: "#" }
+    {
+        title: "Project One",
+        desc: "A cool project about ...",
+        image: "/img1.png",
+        link: "#",
+    },
+    {
+        title: "Project Two",
+        desc: "Another project ...",
+        image: "/img2.png",
+        link: "#",
+    },
 ];
 
 const settings = {
@@ -15,27 +25,32 @@ const settings = {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1,
-    centerPadding: "0px",
-    responsive: [
-        { breakpoint: 900, settings: { slidesToShow: 1 } }
-    ]
+    arrows: false,
 };
 
 export default function ProjectCarousel() {
     return (
-        <div className="container-project">
-            <h2 className="text-center mb-4">My Projects</h2>
-            <Slider {...settings}>
+        <div className="carousel-wrapper">
+            <div className="carousel-left">
+                <Slider {...settings}>
+                    {projects.map((proj, idx) => (
+                        <div key={idx} className="carousel-image-box">
+                            <img src={proj.image} alt={proj.title} className="carousel-img" />
+                        </div>
+                    ))}
+                </Slider>
+            </div>
+            <div className="carousel-right">
                 {projects.map((proj, idx) => (
-                    <div key={idx} className="project-card">
-                        <img src={proj.image} alt={proj.title} style={{ width: "100%", borderRadius: 8 }} />
+                    <div key={idx} className="carousel-text-block">
                         <h3>{proj.title}</h3>
                         <p>{proj.desc}</p>
-                        <a href={proj.link} target="_blank" rel="noopener noreferrer">View Project</a>
+                        <a href={proj.link} target="_blank" rel="noreferrer">
+                            View Project
+                        </a>
                     </div>
                 ))}
-            </Slider>
+            </div>
         </div>
     );
 }
