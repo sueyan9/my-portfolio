847da8fc32b9a09eaae28b5d23dd8de10046fa28
sueyan9 <409133742@qq.com>
2025-06-16 22:32:18 +1200

    project details

--- src/data/projects.js
+++
@@ -1,9 +0,0 @@
-export default [
-    {
-        title: "Project One",
-        desc: "A cool project about ...",
-        image: "/assets/project1.png",
-        link: "https://github.com/yourname/project1"
-    },
-    // ...更多项目
-];
